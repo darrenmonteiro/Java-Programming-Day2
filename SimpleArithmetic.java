137b6ec9d29d435d6484a0c403057ab1d6ac5229
darrenmonteiro <43502438+darrenmonteiro@users.noreply.github.com>
2018-09-29 12:24:08 -0700

    First Commit
    First ever commit

---
+++ SimpleArithmetic.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class day2{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the first number:");
+    int n1 = input.nextInt();
+    System.out.println("Enter the second number:");
+    int n2 = input.nextInt();
+    System.out.println (n1 + n2);
+  }
+}
